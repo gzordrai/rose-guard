56d06e237055500d7db0ff00ad64d3c4ab2181ce
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-08-30 19:02:21 +0200

    feat: implement app sidebar

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,13 +1,15 @@
 import { invoke } from "@tauri-apps/api/core";
 import { useEffect, useState } from "react";
 import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
-import { ApiContainerSummary } from "@/types/api";
+import { ContainerResponse, ContainerSummary } from "@/types/api";
 
 export function AppSidebar() {
-  const [containers, setContainers] = useState([]);
+  const [containers, setContainers] = useState<ContainerSummary[]>([]);
 
   async function getContainers() {
-    setContainers(await invoke("get_containers"))
+    invoke<ContainerResponse>("get_containers")
+      .then(response => setContainers(response.items))
+      .catch(_ => setTimeout(getContainers, 5000));
   }
 
   useEffect(() => {
@@ -20,11 +22,11 @@ export function AppSidebar() {
         <SidebarGroup>
           <SidebarGroupLabel>Servers</SidebarGroupLabel>
           <SidebarMenu>
-            {containers.map((container: ApiContainerSummary) =>
-              <SidebarMenuItem key={container.name}>
+            {containers.map((container: ContainerSummary) =>
+              <SidebarMenuItem key={container.id}>
                 <SidebarMenuButton>
                   <a href="">
-                    -- todo add status icon
+                    {/* todo add status icon */}
                     <span>{container.name}</span>
                   </a>
                 </SidebarMenuButton>
