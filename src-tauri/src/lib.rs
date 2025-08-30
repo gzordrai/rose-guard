ce7a001a30b1a4a4f11713f171d818944bbc6bc2
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-08-30 19:03:25 +0200

    feat: implement tauri command to fetch containers for the sidebar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,50 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ContainersResponse {
+    items: Vec<ApiContainerSummary>,
+    count: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+/// Represents a minimal version of `ContainerSummary` from Bollard, designed to avoid sending unnecessary data in API responses.
+pub struct ApiContainerSummary {
+    /// The unique identifier of the Docker container.
+    pub id: String,
+
+    /// The name of the Docker container.
+    pub name: String,
+
+    /// The image used to create the container.
+    pub image: String,
+
+    /// The current state of the container (e.g., running, exited).
+    pub state: String,
+
+    /// The status description of the container (e.g., "Up 5 minutes").
+    pub status: String,
+
+    /// The creation timestamp of the container (Unix time).
+    pub created: i64,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_containers() -> Result<ContainersResponse, String> {
+    let resp = reqwest::get("http://127.0.0.1:3000/containers")
+        .await
+        .map_err(|e| e.to_string())?
+        .json::<ContainersResponse>()
+        .await
+        .map_err(|e| e.to_string())?;
+
+    Ok(resp)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_containers])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
