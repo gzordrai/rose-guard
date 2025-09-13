2f2db68f4e979421524bfdb3d483234599186ccc
gzordrai <40698142+gzordrai@users.noreply.github.com>
2025-09-13 14:29:05 +0200

    refactor: use rose-bound instead of having duplicated types

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,47 +1,4 @@
-use serde::{Deserialize, Serialize};
-
-// Maybe do a crate for common types between backend and frontend?
-#[derive(Copy, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
-enum Status {
-    Ok,
-    Down,
-}
-
-#[derive(Serialize, Deserialize)]
-struct HealthResponse {
-    status: Status,
-    version: String,
-    uptime_seconds: u64,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct ContainersResponse {
-    items: Vec<ApiContainerSummary>,
-    count: usize,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-/// Represents a minimal version of `ContainerSummary` from Bollard, designed to avoid sending unnecessary data in API responses.
-pub struct ApiContainerSummary {
-    /// The unique identifier of the Docker container.
-    pub id: String,
-
-    /// The name of the Docker container.
-    pub name: String,
-
-    /// The image used to create the container.
-    pub image: String,
-
-    /// The current state of the container (e.g., running, exited).
-    pub state: String,
-
-    /// The status description of the container (e.g., "Up 5 minutes").
-    pub status: String,
-
-    /// The creation timestamp of the container (Unix time).
-    pub created: i64,
-}
+use rose_bound::{ContainersResponse, HealthResponse};
 
 // use real error handling later instead of just String
 #[tauri::command]
@@ -72,7 +29,7 @@ async fn get_containers() -> Result<ContainersResponse, String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_containers])
+        .invoke_handler(tauri::generate_handler![get_health, get_containers])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
